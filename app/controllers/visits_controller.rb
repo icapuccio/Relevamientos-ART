c0e403d39ac28b009b906ec1d3ba2047064636b9
lgrula <lucas.grula@mercadolibre.com>
2017-08-22 00:24:04 -0300

    Se agrega endpoint -> Visita show

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -10,6 +10,33 @@ class VisitsController < ApplicationController
     end
   end
 
+  def show
+    @visit = Visit.find(params[:id])
+    respond_to do |format|
+      if @visit.nil?
+        show_not_found_format(format)
+      else
+        show_format_ok(format)
+      end
+    end
+  end
+
+  def show_not_found_format(format)
+    format.json do
+      render json: @visit, status: :not_found
+    end
+    format.html { redirect_to @visits, notice: 'Visit not found' }
+    format.xml { redirect_to @visits, notice: 'Visit not found' }
+  end
+
+  def show_format_ok(format)
+    format.json do
+      render json: @visit, status: :ok
+    end
+    format.html
+    format.xml
+  end
+
   # status param is optional
   # user_id param is optional
   def valid_params?
